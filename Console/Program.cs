f65fe7a70fa2e09da60169e03a554f92e916b7b6
Richard Harper <richard.harper@americanfinancing.net>
2017-11-09 12:48:57 -0700

    Update console message

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Console
 {
@@ -10,7 +6,7 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine($"Welcome to Demo.Console.\nCurrent date is {DateTime.Now}.\nPress <any> key to exit");
+            System.Console.WriteLine($"Welcome to Demo.Console.\nCurrent date is {DateTime.Now}.\nAnother version.\nPress <any> key to exit");
 
             System.Console.ReadKey();
         }
